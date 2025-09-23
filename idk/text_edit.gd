e839189f1731fb40af54bad654dd7654ffa5a503
iluebke862 <iluebke862@phm.k12.in.us>
2025-09-23 11:23:49 -0400

    Add files via upload

---
+++ idk/text_edit.gd
@@ -0,0 +1,12 @@
+extends TextEdit
+var test
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	test = get_node("idiot(vhan)")
+	print(test)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
